52d63a251fdd82bb16819d0e5cd25d35542a5742
Jerry242429 <eyerusalem008@gmail.com>
2024-02-02 14:32:19 -0800

    binary

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,36 @@
+include "binary_trees.h"
+
+/**
+ * binary_trees_ancestor - ancestor
+ * @first: First n
+ * @second: Second n
+ * Return: ancestor node
+ *
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *b, *r;
+
+	if (first == NULL || second == NULL)
+	{
+		return (NULL);
+	}
+	if (first == second)
+	{
+		return ((binary_tree_t *)first);
+	}
+
+	b = first->parent;
+	r = second->parent;
+	if (b == NULL || first == r || (!b->parent && r))
+	{
+		return (binary_trees_ancestor(first, r));
+	}
+	else if (r == NULL || b == second || (!r->parent && b))
+	{
+		return (binary_trees_ancestor(b, second));
+	}
+	return (binary_trees_ancestor(b, r));
+}
