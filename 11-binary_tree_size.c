a44c4e5a20fcae060a21886384504fa71a7b35ce
Jerry242429 <eyerusalem008@gmail.com>
2024-02-02 13:50:05 -0800

    binary

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,13 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - function that return the size of a tree
- * @tree: tree to check
- * Return: size of tree
+ * binary_tree_size -  the size of a tree returned
+ * @tree: tree
+ * Return: size
+ *
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t size = 0, r = 0, l = 0;
+	size_t s = 0, n = 0, b = 0;
 
 	if (tree == NULL)
 	{
@@ -14,9 +15,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	}
 	else
 	{
-		l = binary_tree_size(tree->left);
-		r = binary_tree_size(tree->right);
-		size = r + l + 1;
+		b = binary_tree_size(tree->left);
+		n = binary_tree_size(tree->right);
+		s = r + l + 1;
 	}
-	return (size);
+	return (s);
 }
