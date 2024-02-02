0b779e5d380dae1f998be6844a403c1bc7e990d6
Jerry242429 <eyerusalem008@gmail.com>
2024-02-02 13:55:23 -0800

    binary

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,13 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_nodes - function that returns # of nodes in a tree with children
- * @tree: tree to check
- * Return: number of nodes with children
+ * binary_tree_nodes - nodes in a tree with children
+ * @tree: tree 
+ * Return: number of nodes
+ *
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 
-	size_t  node = 0;
+	size_t  n = 0;
 
 	if (tree == NULL)
 	{
@@ -15,9 +16,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	}
 	else
 	{
-		node += ((tree->left || tree->right) ? 1 : 0);
-		node += binary_tree_nodes(tree->left);
-		node += binary_tree_nodes(tree->right);
-		return (node);
+		n += ((tree->left || tree->right) ? 1 : 0);
+		n += binary_tree_nodes(tree->left);
+		n += binary_tree_nodes(tree->right);
+		return (n);
 	}
 }
