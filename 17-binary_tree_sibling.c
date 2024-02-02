739897dbcd9b62722d2a0a1c1f7a02fec03a249e
Jerry242429 <eyerusalem008@gmail.com>
2024-02-02 14:26:07 -0800

    binary

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,9 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_sibling - function that finds if a node is sibling
- * @node: node to check
- * Return: The sibling node
+ * binary_tree_sibling - if a node is sibling
+ * @node: node
+ * Return: sibling node
+ *
  */
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
