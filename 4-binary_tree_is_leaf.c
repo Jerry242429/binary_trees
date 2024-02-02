85fb93c48247afde9593bc2d2fc11cc69cd9f8f2
Nardos <contactnardos@gmail.com>
2024-02-02 21:22:52 +0000

    4-binary_tree_is_leaf.c

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_leaf - know if a node is a leaf, that means that the node
- * does not have any child neither left nor right
+ * binary_tree_is_leaf - is a leaf, that means that the node
+ * does not have any neither left nor right
  * @node: node to study
- * Return: 1 if it is a leaf or 0 if it is not
+ * Return: 1 a leaf or 0 if it is not
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
